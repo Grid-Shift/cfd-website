8363ac07ffd02cf61a802b889c44391b53b08dc7
Ezekiel Binion <ebinion@gmail.com>
2018-04-20 13:44:16 -0500

    feat: install gatsby

---
+++ gatsby/src/layouts/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+
+import Header from '../components/header'
+import '../styles/screen.scss'
+
+const Layout = ({ children, data }) => (
+  <div>
+    <Helmet>
+      <title>{data.site.siteMetadata.title}</title>
+      <meta name='description' content='Sample' />
+      <meta name='keywords' content='sample, something' />
+    </Helmet>
+
+    <Header siteTitle={data.site.siteMetadata.title} />
+    <div>
+      {children()}
+    </div>
+  </div>
+)
+
+Layout.propTypes = {
+  children: PropTypes.func,
+}
+
+export default Layout
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
