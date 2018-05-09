cc67b8d182e97e51512f56e1cd3461e7e93915d9
Ezekiel Binion <ebinion@gmail.com>
2018-05-09 16:31:16 -0500

    feat: remove Middleman site

--- middleman/lib/isProd.js
+++
@@ -1,3 +0,0 @@
-module.exports = () => {
-  return process.env.NODE_ENV === 'production'
-}
--- middleman/webpack.config.js
+++
@@ -1,5 +0,0 @@
-const isProd = require('./lib/isProd')
-
-module.exports = () => {
-  return require(`./webpack.${isProd() ? 'prod' : 'dev'}.js`)
-}
