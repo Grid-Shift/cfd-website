93e56044f76f36c9c4b0a43f3f8e097b351b0313
Ezekiel Binion <ebinion@gmail.com>
2018-05-22 17:07:59 -0500

    WIP

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 const getUri = require('./lib/getUri')
+const secrets = require('./.secrets.json')
 
 module.exports = {
   siteMetadata: {
@@ -10,17 +11,17 @@ module.exports = {
     {
       resolve: `gatsby-source-prismic`,
       options: {
-        repositoryName: `cfdwebsite`,
-        accessToken: `MC5XdHFTdmlzQUFDc0EwMDhs.77-9CO-_vVV777-977-977-9VDQiKnAgfwXvv73vv73vv71-Cmnvv73vv73vv70r77-977-9OTdD77-9`,
-        linkResolver: ({ node, key, value }) => doc => {
-          // TODO: Implement link resolver
-          // Check documentation: https://prismic.io/docs/javascript/beyond-the-api/link-resolving
-          return getUri(doc)
-        },
-        htmlSerializer: ({ node, key, value }) => (element, content) => {
-          // TODO: Consider implementing an HTML serializer
-          // Check the documentation: https://prismic.io/docs/javascript/beyond-the-api/html-serializer
-        }
+        repositoryName: secrets.prismic.repositoryName,
+        accessToken: secrets.prismic.accessToken,
+        // linkResolver: ({ node, key, value }) => doc => {
+        //   // TODO: Implement link resolver
+        //   // Check documentation: https://prismic.io/docs/javascript/beyond-the-api/link-resolving
+        //   return getUri(doc)
+        // },
+        // htmlSerializer: ({ node, key, value }) => (element, content) => {
+        //   // TODO: Consider implementing an HTML serializer
+        //   // Check the documentation: https://prismic.io/docs/javascript/beyond-the-api/html-serializer
+        // }
       },
     },
   ],
--- gatsby-node.js
+++ gatsby-node.js
@@ -25,77 +25,77 @@ const templatePath = {
 /* -----------------------------------------------------------------------------
     !-- Add slugs to Nodes
   --------------------------------------------------------------------------- */
-exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
-  const { createNodeField } = boundActionCreators
-
-  if(node.internal.type === 'PrismicDocument') {
-    let slug = getUri(node)
-
-    if(slug) {
-      createNodeField({
-        node,
-        name: 'slug',
-        value: slug
-      })
-    }
-  }
-}
+// exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+//   const { createNodeField } = boundActionCreators
+//
+//   if(node.internal.type === 'PrismicDocument') {
+//     let slug = getUri(node)
+//
+//     if(slug) {
+//       createNodeField({
+//         node,
+//         name: 'slug',
+//         value: slug
+//       })
+//     }
+//   }
+// }
 
 /* -----------------------------------------------------------------------------
     !-- Dynamically Create Pages
   --------------------------------------------------------------------------- */
 
-exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
-
-  return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allPrismicDocument {
-          edges {
-            node {
-              type
-              uid
-              fields {
-                slug
-              }
-            }
-          }
-        }
-      }
-    `).then(result => {
-      result.data.allPrismicDocument.edges.forEach(({ node }) => {
-        let componentPath
-
-        switch (node.type) {
-           case prismicType.POSTS:
-             componentPath = templatePath.POSTS
-             break
-           case prismicType.CATS:
-             componentPath = templatePath.CATS
-             break
-           case prismicType.AUTHORS:
-             componentPath = templatePath.AUTHORS
-             break
-           default:
-             componentPath = false
-        }
-
-        if (componentPath) {
-          createPage({
-            path: node.fields.slug,
-            component: path.resolve(componentPath),
-            context: {
-              // Data passed to context is available in page queries as GraphQL variables.
-              uid: node.uid,
-            },
-          })
-        }
-
-      })
-      resolve()
-    }).catch(error => {
-      reject()
-    })
-  })
-}
+// exports.createPages = ({ graphql, boundActionCreators }) => {
+//   const { createPage } = boundActionCreators
+//
+//   return new Promise((resolve, reject) => {
+//     graphql(`
+//       {
+//         allPrismicDocument {
+//           edges {
+//             node {
+//               type
+//               uid
+//               fields {
+//                 slug
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `).then(result => {
+//       result.data.allPrismicDocument.edges.forEach(({ node }) => {
+//         let componentPath
+//
+//         switch (node.type) {
+//            case prismicType.POSTS:
+//              componentPath = templatePath.POSTS
+//              break
+//            case prismicType.CATS:
+//              componentPath = templatePath.CATS
+//              break
+//            case prismicType.AUTHORS:
+//              componentPath = templatePath.AUTHORS
+//              break
+//            default:
+//              componentPath = false
+//         }
+//
+//         if (componentPath) {
+//           createPage({
+//             path: node.fields.slug,
+//             component: path.resolve(componentPath),
+//             context: {
+//               // Data passed to context is available in page queries as GraphQL variables.
+//               uid: node.uid,
+//             },
+//           })
+//         }
+//
+//       })
+//       resolve()
+//     }).catch(error => {
+//       reject()
+//     })
+//   })
+// }
